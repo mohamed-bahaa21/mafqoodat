14a22ac7182d3ee01cc36588ee0f159d424a52c1
MohammadBahaa1 <MohammadBahaa_2001mb@outlook.com>
2020-03-17 17:06:50 +0200

    show user profile

--- app/src/main/java/com/project/firebasesocial/LoginActivity.java
+++ app/src/main/java/com/project/firebasesocial/LoginActivity.java
@@ -246,24 +246,28 @@ public class LoginActivity extends AppCompatActivity {
                     // Sign in success, update UI with the signed-in user's information
                     FirebaseUser user = mAuth.getCurrentUser();
 
-                    // get user email & uid from auth
-                    String email = user.getEmail();
-                    String uid = user.getUid();
-                    //when user is registered store user info in firebase realtime database too using hashmap
-                    HashMap<Object, String> hashMap = new HashMap<>();
-                    // put info in hashmap
-                    hashMap.put("email", email);
-                    hashMap.put("uid", uid);
-                    hashMap.put("name", ""); // will add later (e.g. edit profile)
-                    hashMap.put("phone", ""); // will add later (e.g. edit profile)
-                    hashMap.put("image", ""); // will add later (e.g. edit profile)
-                    //firebase database instance
-                    FirebaseDatabase database = FirebaseDatabase.getInstance();
-                    // path to store user data named "Users"
-                    DatabaseReference reference = database.getReference("Users");
-                    //put data within hashmap in database
-                    reference.child(uid).setValue(hashMap);
+                    //if user is signing in first time then get and show user info from google account
+                    if(task.getResult().getAdditionalUserInfo().isNewUser()) {
+                        // get user email & uid from auth
+                        String email = user.getEmail();
+                        String uid = user.getUid();
+                        //when user is registered store user info in firebase realtime database too using hashmap
+                        HashMap<Object, String> hashMap = new HashMap<>();
+                        // put info in hashmap
+                        hashMap.put("email", email);
+                        hashMap.put("uid", uid);
+                        hashMap.put("name", ""); // will add later (e.g. edit profile)
+                        hashMap.put("phone", ""); // will add later (e.g. edit profile)
+                        hashMap.put("image", ""); // will add later (e.g. edit profile)
+                        //firebase database instance
+                        FirebaseDatabase database = FirebaseDatabase.getInstance();
+                        // path to store user data named "Users"
+                        DatabaseReference reference = database.getReference("Users");
+                        //put data within hashmap in database
+                        reference.child(uid).setValue(hashMap);
+                    }
 
+                    //show user email in toast
                     Toast.makeText(LoginActivity.this, ""+user.getEmail(), Toast.LENGTH_SHORT).show();
                     startActivity(new Intent(LoginActivity.this, DashboardActivity.class));
                     finish();
--- app/src/main/java/com/project/firebasesocial/ProfileFragment.java
+++ app/src/main/java/com/project/firebasesocial/ProfileFragment.java
@@ -1,18 +1,39 @@
 package com.project.firebasesocial;
 
+
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProfileFragment extends Fragment {
+
+    FirebaseAuth firebaseAuth;
+    FirebaseUser user;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+
+    ImageView avatarIv;
+    TextView nameTv, emailTv, phoneTv;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -20,9 +41,50 @@ public class ProfileFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference("Users");
+
+        avatarIv = view.findViewById(R.id.avatarIv);
+        nameTv = view.findViewById(R.id.nameTv);
+        emailTv = view.findViewById(R.id.emailTv);
+        phoneTv = view.findViewById(R.id.phoneTv);
+
+        Query query = databaseReference.orderByChild("email").equalTo(user.getEmail());
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds : dataSnapshot.getChildren()){
+                    String name = "" + ds.child("name").getValue();
+                    String email = "" + ds.child("email").getValue();
+                    String phone = "" + ds.child("phone").getValue();
+                    String image = "" + ds.child("image").getValue();
+
+                    nameTv.setText(name);
+                    emailTv.setText(email);
+                    phoneTv.setText(phone);
+
+                    try {
+                        // if image is received then set
+                        Picasso.get().load(image).into(avatarIv);
+                    } catch (Exception e){
+                        //if there is any exception while getting image then set default
+                        Picasso.get().load(R.drawable.ic_add_image).into(avatarIv);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        return view;
     }
 }
