b0cbe621e1240398b0ea8cecb4d9b362c8fd14b3
MohammadBahaa1 <MohammadBahaa_2001mb@outlook.com>
2020-03-25 06:46:47 +0200

    26 Chat List

--- app/src/main/java/com/project/firebasesocial/ChatListFragment.java
+++ app/src/main/java/com/project/firebasesocial/ChatListFragment.java
@@ -7,6 +7,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.view.MenuItemCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.text.TextUtils;
 import android.view.LayoutInflater;
@@ -19,24 +20,132 @@ import android.widget.SearchView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.project.firebasesocial.adapters.AdapterChat;
+import com.project.firebasesocial.adapters.AdapterChatlist;
+import com.project.firebasesocial.models.ModelChat;
+import com.project.firebasesocial.models.ModelChatlist;
+import com.project.firebasesocial.models.ModelUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChatListFragment extends Fragment {
 
     FirebaseAuth firebaseAuth;
+    RecyclerView recyclerView;
+    List<ModelChatlist> chatlistList;
+    List<ModelUser> userList;
+    DatabaseReference reference;
+    FirebaseUser currentUser;
+    AdapterChatlist adapterChatlist;
 
     public ChatListFragment() {
         // Required empty public constructor
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_chat_list, container, false);
 
         firebaseAuth = FirebaseAuth.getInstance();
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        recyclerView = view.findViewById(R.id.recyclerView);
+
+        chatlistList = new ArrayList<>();
+
+        reference = FirebaseDatabase.getInstance().getReference("Chatlist").child(currentUser.getUid());
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                chatlistList.clear();
+                for (DataSnapshot ds: dataSnapshot.getChildren()){
+                    ModelChatlist chatlist = ds.getValue(ModelChatlist.class);
+                    chatlistList.add(chatlist);
+                }
+                loadChats();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
         return view;
+    }
+
+    private void loadChats() {
+        userList = new ArrayList<>();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                userList.clear();
+                for (DataSnapshot ds: dataSnapshot.getChildren()){
+                    ModelUser user = ds.getValue(ModelUser.class);
+                    for (ModelChatlist chatlist: chatlistList){
+                        if (user.getUid() != null && user.getUid().equals(chatlist.getId())){
+                            userList.add(user);
+                            break;
+                        }
+                    }
+                    //adapter
+                    adapterChatlist = new AdapterChatlist(getContext(), userList);
+                    //set adapter
+                    recyclerView.setAdapter(adapterChatlist);
+                    //set last message
+                    for (int i=0; i<userList.size(); i++){
+                        lastMessage(userList.get(i).getUid());
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void lastMessage(final String userId) {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Chats");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String theLastMessage = "default";
+                for (DataSnapshot ds: dataSnapshot.getChildren()){
+                    ModelChat chat = ds.getValue(ModelChat.class);
+                    if (chat==null){
+                        continue;
+                    }
+                    String sender = chat.getSender();
+                    String receiver = chat.getReceiver();
+                    if (sender == null || receiver == null){
+                        continue;
+                    }
+                    if (chat.getReceiver().equals(currentUser.getUid()) &&
+                            chat.getSender().equals(userId) ||
+                    chat.getReceiver().equals(userId) &&
+                    chat.getSender().equals(currentUser.getUid())){
+                        theLastMessage = chat.getMessage();
+                    }
+                }
+                adapterChatlist.setLastMessageMap(userId, theLastMessage);
+                adapterChatlist.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     private void checkUserStatus(){
---
+++ app/src/main/java/com/project/firebasesocial/adapters/AdapterChatlist.java
@@ -0,0 +1,118 @@
+package com.project.firebasesocial.adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.project.firebasesocial.ChatActivity;
+import com.project.firebasesocial.R;
+import com.project.firebasesocial.models.ModelUser;
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class AdapterChatlist extends RecyclerView.Adapter<AdapterChatlist.MyHolder>  {
+
+    Context context;
+    List<ModelUser> userList; //get user info
+    private HashMap<String, String> lastMessageMap;
+
+    //constructor
+    public AdapterChatlist(Context context, List<ModelUser> userList) {
+        this.context = context;
+        this.userList = userList;
+        this.lastMessageMap = new HashMap<>();
+    }
+
+    @NonNull
+    @Override
+    public MyHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        //inflate layout row_chatlist.xml
+        View view = LayoutInflater.from(context).inflate(R.layout.row_chatlist, viewGroup, false);
+        return new MyHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyHolder myHolder, int i) {
+        //get data
+        final String hisUid = userList.get(i).getUid();
+        String userImage = userList.get(i).getImage();
+        String userName = userList.get(i).getName();
+        String lastMessage = lastMessageMap.get(hisUid);
+
+
+        //set data
+        myHolder.nameTv.setText(userName);
+        // the error is: lastMessage here equals null, how-to-solve ?
+        Toast.makeText(context, "LastMessage: "+lastMessage, Toast.LENGTH_SHORT).show();
+        lastMessage = "default";
+
+        if (lastMessage != null || lastMessage.equals("default")){
+            myHolder.lastMessageTv.setVisibility(View.GONE);
+        } else {
+            myHolder.lastMessageTv.setVisibility(View.VISIBLE);
+            myHolder.lastMessageTv.setText(lastMessage);
+        }
+
+        try {
+            Picasso.get().load(userImage).placeholder(R.drawable.ic_default_img).into(myHolder.profileIv);
+        } catch (Exception e) {
+            Picasso.get().load(R.drawable.ic_default_img).into(myHolder.profileIv);
+        }
+
+        //set online status of other users in chatlist
+        if (userList.get(i).getOnlineStatus().equals("online")){
+            //online
+            myHolder.onlineStatusIv.setImageResource(R.drawable.circle_online);
+        } else {
+            //offline
+            myHolder.onlineStatusIv.setImageResource(R.drawable.circle_offline);
+        }
+
+        //handle click of user in chatlist
+        myHolder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //start chat activity with that user
+                Intent intent = new Intent(context, ChatActivity.class);
+                intent.putExtra("hisUid", hisUid);
+                context.startActivity(intent);
+            }
+        });
+    }
+
+    public void setLastMessageMap(String userId, String lastMessage) {
+        lastMessageMap.put(userId, lastMessage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return userList.size(); //size of the list
+    }
+
+    class MyHolder extends RecyclerView.ViewHolder {
+        //views of row_chatlist.xml
+        ImageView profileIv, onlineStatusIv;
+        TextView nameTv, lastMessageTv;
+
+        public MyHolder(@NonNull View itemView) {
+            super(itemView);
+
+            //init views
+            profileIv = itemView.findViewById(R.id.profileIv);
+            onlineStatusIv = itemView.findViewById(R.id.onlineStatusIv);
+            nameTv = itemView.findViewById(R.id.nameTv);
+            lastMessageTv = itemView.findViewById(R.id.lastMessageTv);
+        }
+    }
+
+}
